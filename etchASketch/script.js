bf158e65412fdc88b3f1ae21d76d2a223defd331
Mohamed Wasim Mohamed Akram <63498839+MohamedWasimMohamedAkram@users.noreply.github.com>
2021-05-04 04:50:52 +0300

    Added completed etchASketch files

--- etchASketch/script.js
+++ etchASketch/script.js
@@ -1,7 +1,7 @@
 function setBox(size){
 const container = document.getElementById("mainContainer");
-var all = document.getElementsByClassName('element');
-var reset = document.getElementById("resetButton");
+let all = document.getElementsByClassName('element');
+let reset = document.querySelector('.resetButton');
 //const div = document.createElement("div");
 //div.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, 50px); grid-template-rows: repeat(16, 50px); width: auto; height: auto; border: 1px solid black');
 //div.classList.add("grid-container");
@@ -26,19 +26,16 @@ document.addEventListener("mouseover", (e) => {
     var z = Math.floor(Math.random() * 256);
     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
     e.target.setAttribute("style", `background-color: ${bgColor} ; border: 1px solid black`);
-})
+});
 
-document.addEventListener("click", (e) => {
-    let userPrompt = prompt("Enter dimension (1 to 100): ");
+reset.addEventListener("click", (e) => {
     let buttonId = e.target.className;
     if(buttonId != "resetButton"){
         return;
     }
-    let boxSize = 960/userPrompt + "px";
-    container.setAttribute("style", `display: grid; grid-template-columns: repeat(${userPrompt}, ${boxSize}); grid-template-rows: repeat(${userPrompt}, ${boxSize}); width: 960px; height: 960px; margin: auto`);
-    for (var i = 0; i < all.length; i++) {
-        all[i].setAttribute("style", "border: 1px solid black");
-    }
+    else{
+    window.location.reload();
+}
 })
 }
 let userPrompt = prompt("Enter dimension (1 to 100): ");
